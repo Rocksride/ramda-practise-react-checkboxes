d87007c55c4d1948516f981dddafa1bf726ebdd8
Rocksride <ihavenonameTaras@gmail.com>
2018-07-24 03:52:15 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const h3 = title => <h3>{title}</h3>
+const app = () => 
+  <main>
+    {h3('hellow fellas')}
+  </main>
+
+
+export default app;
